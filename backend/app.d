1cf947caa95ad903985d853f5fde506ead6ccbcf
Jan Allersma <jan@allersma.be>
2023-10-28 21:22:02 +0200

    Add logic to get mouse position and click

--- backend/app.d
+++ backend/app.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.string;
+import std.conv;
 
 extern (C++) void init(int width, int height);
 extern (C++) void drawRect(int x, int y, int width, int height);
@@ -9,10 +10,19 @@ extern (C++) void fillRect(int x, int y, int width, int height);
 extern (C++) void setColor(int r, int g, int b, int a);
 extern (C++) void createButton(int x, int y, int r, int g, int b, char* text);
 
-void drawGrid(int width, int height) {
+extern (C) int SDL_GetMouseState(int* x, int* y);
+
+// This does not match with specification of SDL. But apparently, it works.
+// Problem could be caused because SDL_GetMouseState returns UInt32 (encoded C thingy)
+enum int leftClick = 1;
+enum int rightClick = 4;
+
+enum int userClosedWindowSignal = 0;
+
+void drawGrid(int offsetX, int offsetY, int width, int height) {
 	for (int x = 0; x < width; x++) {
 		for (int y = 0; y < height; y++) {
-			drawRect(x * 20, y * 20, 20, 20);
+			drawRect(x * 20 + offsetX, y * 20 + offsetY, 20, 20);
 		}
 	}
 }
@@ -30,7 +40,7 @@ void main() {
 
 	// Draw grid
 	setColor(0xC0,0xC0,0xE0,0xff);
-    drawGrid(600 / 20, 600 / 20);
+    drawGrid(0, 0, 600 / 20, 600 / 20);
 
 	// Create button
 	createButton(650, 50, 0,0,0, cast(char*)toStringz("Text"));
@@ -38,8 +48,18 @@ void main() {
 	redraw();
     short signal = tick();
 	writeln("Test");
+	int[3] mouseState;
+	bool clicked = false;
+	int lastState;
 
-	while (signal == 0) {
+	while (signal == userClosedWindowSignal) {
 		signal = tick();
+		mouseState[0] = SDL_GetMouseState(&mouseState[1], &mouseState[2]);
+		writeln("x: " ~ to!string(mouseState[1]));
+		writeln("y: " ~ to!string(mouseState[2]));
+		if (mouseState[0] == leftClick) clicked = true;
+		if (mouseState[0] != 0) lastState = mouseState[0];
 	}
+	if (clicked) writefln("Clicked! %x", lastState);
+	else writefln("Mouse state: %x", lastState);
 }
